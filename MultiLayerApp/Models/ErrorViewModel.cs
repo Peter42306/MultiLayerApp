c4dcfd9c1e75e256562e999b590c5400967603aa
Petr Zalizko <pzalizko@gmail.com>
2024-06-29 18:51:49 +0300

    Add project files.

---
+++ MultiLayerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultiLayerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
